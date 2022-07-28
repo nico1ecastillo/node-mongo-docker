9b855237ca9a31cccac1ece969c2d386de12a3f6
Nicole Castillo <103956499+nico1ecastillo@users.noreply.github.com>
2022-07-28 14:04:09 -0700

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var { Todos } = require('../models/todos');
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  const todos = await Todos.find();
+  res.render('index', { title: 'Todos', todos: todos });
+});
+
+module.exports = router;
